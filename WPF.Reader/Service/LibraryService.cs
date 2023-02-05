14b0242d62e4bb0c2a0a0baca23a3afe28ce9b73
elassilsalaheddine <87868463+elassilsalaheddine@users.noreply.github.com>
2023-02-05 23:59:27 +0100

    Update LibraryService.cs

--- WPF.Reader/Service/LibraryService.cs
+++ WPF.Reader/Service/LibraryService.cs
@@ -1,11 +1,26 @@
-﻿using System.Collections.ObjectModel;
-using WPF.Reader.Api;
-using WPF.Reader.Model;
+﻿using MyNamespace;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Windows;
+
+
 
 namespace WPF.Reader.Service
 {
     public class LibraryService
     {
+        private readonly HttpClient _httpClient = new()
+        {
+            BaseAddress = new Uri("http://localhost:5000")
+        };
+        public LibraryService()
+        {
+            UpdateBookList();
+            UpdateGenreList();
+        }
+
         // A remplacer avec vos propre données !!!!!!!!!!!!!!
         // Pensé qu'il ne faut mieux ne pas réaffecter la variable Books, mais juste lui ajouter et / ou enlever des éléments
         // Donc pas de LibraryService.Instance.Books = ...
@@ -16,10 +31,47 @@ namespace WPF.Reader.Service
             new Book(),
             new Book()
         };
+        public async void UpdateBookList()
+        {
+            var books = await new BookClient(_httpClient).GetBookAsync(null);
+
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                Books?.Clear();
+
+                foreach (var book in books.OrderBy(book => book.Id))
+                {
+                    var newbook = new Book()
+                    {
+                      
+                        Price = book.Price,
+                        Genres = book.Genres,
+                        Id = book.Id,
+                        Name = book.Name
+                    };
+                    Books?.Add(newbook);
+                }
+            });
+        }
 
         // C'est aussi ici que vous ajouterez les requête réseau pour récupérer les livres depuis le web service que vous avez fait
         // Vous pourrez alors ajouter les livres obtenu a la variable Books !
         // Faite bien attention a ce que votre requête réseau ne bloque pas l'interface 
+        public ObservableCollection<Genre> Genres { get; set; } = new ObservableCollection<Genre>();
 
+        
+
+        public async void UpdateGenreList()
+        {
+            var genres = await new BookClient(_httpClient).GetGenresAsync();
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                Genres?.Clear();
+                foreach (var genre in genres.ToList())
+                {
+                    Genres?.Add(genre);
+                }
+            });
+        }
     }
 }
